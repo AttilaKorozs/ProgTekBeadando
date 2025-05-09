b29b9c567d38ad2e3b6d939936b2d0d8f1b5c48d
AttilaKorozs <korozs.attila92@gmail.com>
2025-05-09 12:30:44 +0200

    create User DAO functions

--- RSS_feeder/src/main/java/org/rssreader/dao/UserDAO.java
+++ RSS_feeder/src/main/java/org/rssreader/dao/UserDAO.java
@@ -2,15 +2,15 @@ package org.rssreader.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.rssreader.models.User;
 
 public class UserDAO {
 
-
     public static void addUser(User user) {
-        String sql = "INSERT INTO User (username, password, email) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO User (username, password_hash, email) VALUES (?, ?, ?)";
         String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
 
         try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -25,4 +25,59 @@ public class UserDAO {
         }
     }
 
+    public static User authUser(User user) {
+        String sql = "SELECT password_hash, email FROM User WHERE username = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, user.getUsername());
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    String storedHash = rs.getString("password_hash");
+                    if (BCrypt.checkpw(user.getPassword(), storedHash)) {
+                        User authedUser = new User(user.getUsername(), user.getPassword(), rs.getString("email"));
+                        return authedUser;
+                    }
+                }
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean deleteUser(User user) {
+        String sql = "DELETE FROM User WHERE username = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, user.getUsername());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean modifyUser(User user) {
+        String sql = "UPDATE User SET password_hash = ?, email = ? WHERE username = ?";
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, hashedPassword);
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getUsername());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 }
