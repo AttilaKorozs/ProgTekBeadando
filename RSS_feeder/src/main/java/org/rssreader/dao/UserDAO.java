a504e3ca5edf3709abf857cc47138924c74e2491
AttilaKorozs <korozs.attila92@gmail.com>
2025-05-09 10:34:10 +0200

    create User dao class

--- RSS_feeder/src/main/java/org/rssreader/dao/UserDAO.java
+++ RSS_feeder/src/main/java/org/rssreader/dao/UserDAO.java
@@ -1,5 +1,28 @@
 package org.rssreader.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.rssreader.models.User;
+
 public class UserDAO {
+
+
+    public static void addUser(User user) {
+        String sql = "INSERT INTO User (username, password, email) VALUES (?, ?, ?)";
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, hashedPassword);
+            stmt.setString(3, user.getEmail());
+
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
