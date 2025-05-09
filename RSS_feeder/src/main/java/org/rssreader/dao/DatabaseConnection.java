59986f0b4d87a897ca516438b14bbe6f895461b6
AttilaKorozs <korozs.attila92@gmail.com>
2025-05-09 12:31:25 +0200

    add DatabaseConnection Error message

--- RSS_feeder/src/main/java/org/rssreader/dao/DatabaseConnection.java
+++ RSS_feeder/src/main/java/org/rssreader/dao/DatabaseConnection.java
@@ -1,5 +1,6 @@
 package org.rssreader.dao;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -13,15 +14,16 @@ public class DatabaseConnection {
 
     private static Connection connection;
 
-    public static Connection getConnection() throws SQLException, IOException {
+    public static Connection getConnection() throws SQLException, FileNotFoundException {
         if (connection == null || connection.isClosed()) {
             Properties props = new Properties();
             try (InputStream input = App.class.getClassLoader().getResourceAsStream("dbConfig.properties")) {
                 if (input == null) {
-                    System.out.println("Nem található a konfigurációs fájl.");
-                    return null;
+                    throw new IOException();
                 }
                 props.load(input);
+            } catch (IOException e) {
+                throw new FileNotFoundException("DB Connection Settings file not found.");
             }
             connection = DriverManager.getConnection(props.getProperty("db.url"), props.getProperty("db.username"), props.getProperty("db.password"));
         }
